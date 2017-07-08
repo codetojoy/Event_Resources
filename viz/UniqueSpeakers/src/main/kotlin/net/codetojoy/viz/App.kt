134890eff62e3f12f950198e3a827491cd4d5fb0
Michael Easter <codetojoy@gmail.com>
2017-07-08 09:00:13 -0300

    refactor speakers app

--- viz/UniqueSpeakers/src/main/kotlin/net/codetojoy/viz/App.kt
+++ viz/UniqueSpeakers/src/main/kotlin/net/codetojoy/viz/App.kt
@@ -5,6 +5,8 @@ import java.io.*
 
 import org.apache.commons.csv.*
 
+val DATA_ROWS_TOKEN = "__DATA_ROWS"
+
 fun main(args: Array<String>) {
     val csvFile = args[0]
     val templateHtml = args[1]
@@ -12,11 +14,22 @@ fun main(args: Array<String>) {
 
     val reader = FileReader(csvFile)
     val records = CSVFormat.EXCEL.withHeader().parse(reader).getRecords()
-
     records.reverse()
 
+    val monthOutputs = buildMonthOutputs(records)
+
+    val outputLines = buildOutputLines(monthOutputs, templateHtml)
+
+    File(outputHtml).printWriter().use { out ->
+        outputLines.forEach { out.println(it) }
+    }
+
+    println("Ready.")
+}
+
+fun buildMonthOutputs(records: List<CSVRecord>): List<MonthOutput> {
     var allPeople = setOf<String>()
-    var speakerSanitizer = SpeakerSanitizer()
+    val speakerSanitizer = SpeakerSanitizer()
 
     val monthInputs = records.map { MonthInput(it, speakerSanitizer) }
 
@@ -26,19 +39,17 @@ fun main(args: Array<String>) {
         monthOutput
     }
 
+    return monthOutputs
+} 
+
+fun buildOutputLines(monthOutputs: List<MonthOutput>, templateHtml: String): List<String> {
     val dataRows = monthOutputs.map { 
         "[${formatDate(it.monthInput.date)}, ${it.uniquePeople.size}], // ${it.uniquePeople}"
     }
 
     val newLines: List<List<String>> = File(templateHtml).readLines().map { line ->
-        if (line == "__DATA_ROWS") dataRows else listOf(line) 
+        if (line == DATA_ROWS_TOKEN) dataRows else listOf(line) 
     }
 
-    val outputLines = newLines.flatten()
-
-    File(outputHtml).printWriter().use { out ->
-        outputLines.forEach { out.println(it) }
-    }
-
-    println("Ready.")
+    return newLines.flatten()
 }
